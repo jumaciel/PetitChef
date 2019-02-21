7a1e751de47eb71f9843f1d9f162c5b198e2f943
jumaciel <julianamaciel17@gmail.com>
2019-02-21 15:53:29 -0300

    Adicionar arquivos de projeto.

---
+++ PaginaçaoRegexHardXpath/Models/Receita.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PaginaçaoRegexHardXpath.models
+{
+    class Receita
+    {
+        string Titulo { get; set; }
+        string Nota { get; set; }
+        string Votos { get; set; }
+        string Comentarios { get; set; }
+        string Ameis { get; set; }
+        string Ingredientes { get; set; }
+        string Tipo { get; set; }
+        string Dificuldade { get; set; }
+        string Tempo { get; set; }
+        string Calorias { get; set; }
+        string Link { get; set; }
+
+    }
+}
---
+++ PaginaçaoRegexHardXpath/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace PaginaçaoRegexHardXpath
+{
+    class ProgramaInteiro
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+
+        public virtual void EscreveMensagem()
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
+
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
+
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    class ProgramaNovo : ProgramaInteiro
+    {
+        static void Main(string[] args)
+        {
+
+        }
+
+        public override void EscreveMensagem()
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    class Auxiliar
+    {
+        public void RecebePrograma(ProgramaInteiro inteiro)
+        {
+
+        }
+    }
+}
